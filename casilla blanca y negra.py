615b6836ed850c83451b79d575bf8988d11a2b42
juanhm02 <33682864+juanhm02@users.noreply.github.com>
2017-12-15 10:45:14 +0100

    Add files via upload

---
+++ casilla blanca y negra.py
@@ -0,0 +1,9 @@
+casilla1X=int(input("Primera X:"))
+casilla1Y=int(input("Primera Y:"))
+casilla2X=int(input("Segunda X:"))
+casilla2Y=int(input("Segunda Y:"))
+
+if ((casilla1X+casilla1Y+casilla2X+casilla2X2Y)%2==0):
+  print("color negro")
+else:
+  print("color blanco")
