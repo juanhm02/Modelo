e83dfdd1d8764d6817da577ab8acbc67512aeb0b
juanhm02 <33682864+juanhm02@users.noreply.github.com>
2017-12-15 10:46:50 +0100

    Add files via upload

---
+++ movimiento de torre.py
@@ -0,0 +1,9 @@
+casilla1x=int(input("casilla 1x?"))
+casilla2y=int(input("casilla 2y?"))
+casilla1x=int(input("casilla 1x?"))
+casilla2y=int(input("casilla 2y?"))
+
+if ( casilla1x 4==2 ) or ( casilla2y  8==2 ):
+  print("si")
+else:
+  print("no")
